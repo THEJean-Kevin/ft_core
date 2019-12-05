4d148f6375634299dddce2ae74c64d15560f8dc4
THEJean_Kevin <yoann6defeuro@gmail.com>
2019-12-05 19:48:26 +0100

    Add callback for controlpressed

--- src/client/Game.js
+++ src/client/Game.js
@@ -221,12 +221,28 @@ class Game {
         return IsInputDisabled(2) ? InputMode.MOUSSEANDKEYBOARD : InputMode.GAMEPAD
     }
     /**
-     * @descriptionheck whether a control is currently pressed.
+     * @descriptionheck Execute callback when control is pressed
      * @param {Number} index input group (usually 0)
      * @param {Number} control Control
-     * @returns {Boolean}
+     * @param {Function} callback
+     * @returns {Boolean|Number} Return boolean if callback was not set or the id of callback
      */
-    IsControlPressed(index, control) {
-        return !!IsControlPressed(index, Number(control))
+    OnControlPressed(index,control,callback){
+        if(callback){
+            return setTick(()=>{
+                if(IsControlPressed(index,control)){
+                    callback()
+                }
+            })
+        }else{
+            return !!IsControlPressed(index, control)
+        }
+    }
+    /**
+     * @description clear the callback for OnControlPressed
+     * @param {Number} id 
+     */
+    StopOnControlPressed(id){
+        clearTick(id)
     }
 }
