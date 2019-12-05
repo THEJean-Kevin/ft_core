2b4c6ed9ee98cc2877a4a2fe2fcb5e58d1f80967
samuelds <samuelds.pro@gmail.com>
2019-12-05 19:55:35 +0100

    Update audio

--- src/client/Audio.js
+++ src/client/Audio.js
@@ -6,8 +6,9 @@
  */
 
 class Audio {
+
     /**
-     *@description Play sound at Coord pastebin.com/0neZdsZ5 
+     *@description Play sound at Coord pastebin.com/0neZdsZ5
      * @param {Vector3} position
      * @param {Number} soundId
      * @param {String} SoundName
@@ -15,64 +16,72 @@ class Audio {
      * @returns {Number} sound Id
      */
     PlaySoundAt(position, soundId, SoundName, soundSet) {
-        PlaySoundFromCoord(soundId, SoundName, position.x, position.y, position.z, soundSet ? soundSet : null, false, 0, false)
-        return GetSoundId()
+        PlaySoundFromCoord(soundId, SoundName, position.x, position.y, position.z, soundSet ? soundSet : null, false, 0, false);
+        this.id = GetSoundId();
+        return this.id;
     }
+
     /**
-     * @description play sound near Entity pastebin.com/0neZdsZ5 
-     * @param {Entity} entity 
-     * @param {Number} soundId 
-     * @param {String} soundName 
-     * @param {*} soundSet 
+     * @description play sound near Entity pastebin.com/0neZdsZ5
+     * @param {Entity} entity
+     * @param {Number} soundId
+     * @param {String} soundName
+     * @param {*} soundSet
      * @returns {Number} sound Id
      */
     PlaySoundFromEntity(entity, soundId, soundName, soundSet) {
         PlaySoundFromEntity(soundId, soundName, entity.id, soundSet ? soundSet : null, false, 0);
-        return GetSoundId()
+        this.id = GetSoundId();
+        return this.id;
     }
+
     /**
-     * @description Play sound front end pastebin.com/0neZdsZ5 
-     * @param {Number} soundId 
-     * @param {String} soundName 
-     * @param {*} soundSet 
+     * @description Play sound front end pastebin.com/0neZdsZ5
+     * @param {Number} soundId
+     * @param {String} soundName
+     * @param {*} soundSet
      * @returns {Number} sound Id
      */
     PlaySoundFrontEnd(soundId, soundName, soundSet) {
-        PlaySoundFrontend(soundId, soundName, soundSet ? soundSet : null, false)
-        return GetSoundId()
+        PlaySoundFrontend(soundId, soundName, soundSet ? soundSet : null, false);
+        this.id = GetSoundId();
+        return this.id;
     }
+
     /**
      * @description stop the sound
-     * @param {Number} soundId
      */
-    StopSound(soundId) {
-        StopSound(soundId)
+    StopSound() {
+        StopSound(this.id);
     }
+
     /**
-     * @description release the sound
-     * @param {Number} soundId
-    */
-    ReleaseSound(soundId) {
-        ReleaseSoundId(soundId)
+     * @description Release the sound
+     * @return {void}
+     */
+    ReleaseSound() {
+        ReleaseSoundId(this.id);
     }
+
     /**
      * @description check if the sound is finish
      * @param {Number} soundId
      * @returns {Boolean}
-    */
-    HasSoundFinished(soundId){
-        return !!HasSoundFinished(soundId)
+     */
+    HasSoundFinished() {
+        return HasSoundFinished(this.id);
     }
+
     /**
      * @description Set the audio flag
-     * @param {String|Number} flag for number see the array audioFlag 
-     * @param {*} toggle 
+     * @param {String|Number} flag for number see the array audioFlag
+     * @param {*} toggle
      */
-    SetAudioFlag(flag,toggle){
-        if(typeof flag === 'string'){
-            SetAudioFlag(flag,toggle)
-        }else{
-            SetAudioFlag(this.audioFlags[Number(flag)],toggle)
+    SetAudioFlag(flag, toggle) {
+        if (typeof flag === 'string') {
+            SetAudioFlag(flag, toggle)
+        } else {
+            SetAudioFlag(AudioFlag[Number(flag)], toggle);
         }
     }
 
@@ -82,75 +91,31 @@ class Audio {
      * @param {String} soundName
      * @param {*} soundSet
      */
-    PlaySound(soundId,soundName,soundSet){
-        this.releaseSound(this.playSoundFrontEnd(soundId,soundName,soundSet))
+    PlaySound(soundId, soundName, soundSet) {
+        this.PlaySoundFrontEnd(soundId, soundName, soundSet);
+        this.ReleaseSound();
     }
 
     /**
      * @description play music
      * @param {String} musicfile https://pastebin.com/RzDFmB1W
      */
-    PlayMusic(musicfile){
-        if(this._cachedMusicFile !== null){
-            CancelMusicEvent(this._cachedMusicFile)
+    PlayMusic(musicfile) {
+        if (this.musicFile !== null) {
+            CancelMusicEvent(this.musicFile);
         }
-        this._cachedMusicFile = musicfile
-        TriggerMusicEvent(musicFile)
+        this.musicFile = musicfile;
+        TriggerMusicEvent(musicFile);
     }
 
     /**
      * @description Stop music
-     * @param {string} musicFile https://pastebin.com/RzDFmB1W
      */
-    StopMusic(musicFile){
-        if(!musicFile){
-            CancelMusicEvent(musicFile)
-        }else{
-            if(this._cachedMusicFile){
-                CancelMusicEvent(this._cachedMusicFile)
-                this._cachedMusicFile = null
-            }
+    StopMusic() {
+        if (this.musicFile) {
+            CancelMusicEvent(this.musicFile);
+            this.musicFile = null
         }
     }
-
-    _cachedMusicFile;
-    _audioFlag =  [
-        'ActivateSwitchWheelAudio',
-        'AllowCutsceneOverScreenFade',
-        'AllowForceRadioAfterRetune',
-        'AllowPainAndAmbientSpeechToPlayDuringCutscene',
-        'AllowPlayerAIOnMission',
-        'AllowPoliceScannerWhenPlayerHasNoControl',
-        'AllowRadioDuringSwitch',
-        'AllowRadioOverScreenFade',
-        'AllowScoreAndRadio',
-        'AllowScriptedSpeechInSlowMo',
-        'AvoidMissionCompleteDelay',
-        'DisableAbortConversationForDeathAndInjury',
-        'DisableAbortConversationForRagdoll',
-        'DisableBarks',
-        'DisableFlightMusic',
-        'DisableReplayScriptStreamRecording',
-        'EnableHeadsetBeep',
-        'ForceConversationInterrupt',
-        'ForceSeamlessRadioSwitch',
-        'ForceSniperAudio',
-        'FrontendRadioDisabled',
-        'HoldMissionCompleteWhenPrepared',
-        'IsDirectorModeActive',
-        'IsPlayerOnMissionForSpeech',
-        'ListenerReverbDisabled',
-        'LoadMPData',
-        'MobileRadioInGame',
-        'OnlyAllowScriptTriggerPoliceScanner',
-        'PlayMenuMusic',
-        'PoliceScannerDisabled',
-        'ScriptedConvListenerMaySpeak',
-        'SpeechDucksScore',
-        'SuppressPlayerScubaBreathing',
-        'WantedMusicDisabled',
-        'WantedMusicOnMission',
-      ];
-
 
 }
---
+++ src/client/enum/AudioFlag.js
@@ -0,0 +1,44 @@
+/**
+ * @Project: FivemTools
+ * @Author: Samuelds
+ * @License: GNU General Public License v3.0
+ * @Source: https://github.com/FivemTools/ft_core
+ */
+
+const AudioFlag = new Enum([
+    'ActivateSwitchWheelAudio',
+    'AllowCutsceneOverScreenFade',
+    'AllowForceRadioAfterRetune',
+    'AllowPainAndAmbientSpeechToPlayDuringCutscene',
+    'AllowPlayerAIOnMission',
+    'AllowPoliceScannerWhenPlayerHasNoControl',
+    'AllowRadioDuringSwitch',
+    'AllowRadioOverScreenFade',
+    'AllowScoreAndRadio',
+    'AllowScriptedSpeechInSlowMo',
+    'AvoidMissionCompleteDelay',
+    'DisableAbortConversationForDeathAndInjury',
+    'DisableAbortConversationForRagdoll',
+    'DisableBarks',
+    'DisableFlightMusic',
+    'DisableReplayScriptStreamRecording',
+    'EnableHeadsetBeep',
+    'ForceConversationInterrupt',
+    'ForceSeamlessRadioSwitch',
+    'ForceSniperAudio',
+    'FrontendRadioDisabled',
+    'HoldMissionCompleteWhenPrepared',
+    'IsDirectorModeActive',
+    'IsPlayerOnMissionForSpeech',
+    'ListenerReverbDisabled',
+    'LoadMPData',
+    'MobileRadioInGame',
+    'OnlyAllowScriptTriggerPoliceScanner',
+    'PlayMenuMusic',
+    'PoliceScannerDisabled',
+    'ScriptedConvListenerMaySpeak',
+    'SpeechDucksScore',
+    'SuppressPlayerScubaBreathing',
+    'WantedMusicDisabled',
+    'WantedMusicOnMission',
+]);
