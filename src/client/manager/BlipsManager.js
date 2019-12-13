5d36b227850653d63ff48893cdbdaab21f5dc6da
samuelds <samuelds.pro@gmail.com>
2019-12-13 12:39:10 +0100

    Add blip manager

--- src/client/manager/BlipsManager.js
+++ src/client/manager/BlipsManager.js
@@ -5,3 +5,49 @@
  * @Source: https://github.com/FivemTools/ft_core
 */
 
+class BlipsManager {
+
+    _blips = [];
+
+    /**
+     * @description *
+     * @param {string} name
+     * @param {Blip} blip
+     */
+    Add(name, blip) {
+        this._blips[name] = blip;
+    }
+
+    /**
+     * @description *
+     * @param {string} name
+     */
+    Remove(name) {
+        this._blips[name] = undefined;
+    }
+
+    /**
+     * @description *
+     * @param {string} name
+     */
+    Show(name) {
+        this._blips[name].display = true;
+    }
+
+    /**
+     * @description *
+     * @param {string} name
+     */
+    Hide(name) {
+        this._blips[name].display = false;
+    }
+
+    /**
+     * @description *
+     * @param {string} name
+     */
+    Get(name) {
+        return this._blips[name];
+    }
+
+}
